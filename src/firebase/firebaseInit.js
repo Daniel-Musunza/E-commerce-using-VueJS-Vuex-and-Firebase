c9b8b8aab23bce3fa760f3e9010c7959a76c63b9
Daniel-Musunza <musunzafestus2019@gmail.com>
2023-04-05 12:09:47 +0300

    Initial commit

--- src/firebase/firebaseInit.js
+++ src/firebase/firebaseInit.js
@@ -4,12 +4,12 @@ import "firebase/storage"
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyD7Tc_Asec0wlVwtiNFJmd0R31aoq0ZiBo",
-    authDomain: "the-online-boutique.firebaseapp.com",
-    projectId: "the-online-boutique",
-    storageBucket: "the-online-boutique.appspot.com",
-    messagingSenderId: "623142084824",
-    appId: "1:623142084824:web:90f6caa1fea0c2e69437ad"
+    apiKey: "xxxxxxxxxxxxxxxxxxxxx",
+    authDomain: "xxxxxxxxxxxxxxxxxxx",
+    projectId: "xxxxxxxxxxxxxxx",
+    storageBucket: "xxxxxxxxxxxxxxxxx",
+    messagingSenderId: "xxxxxxxxxxxx",
+    appId: "xxxxxxxxxxxxxxxxxxxxxxx"
   };
 
   const firebaseApp = firebase.initializeApp(firebaseConfig);
